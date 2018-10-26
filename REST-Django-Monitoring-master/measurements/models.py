2638b62ae3f96a776d50aaa1925a965c23267b5b
JuanCamiloMunoz <42190057+JuanCamiloMunoz@users.noreply.github.com>
2018-10-26 00:16:54 -0500

    Update models.py

--- REST-Django-Monitoring-master/measurements/models.py
+++ REST-Django-Monitoring-master/measurements/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Measurement(models.Model):
-    measurement = models.CharField(max_length=50)
-    value = models.FloatField(null=True, blank=True, default=None)
+    Arrendatario = models.CharField(max_length=50)
+    Precio = models.FloatField(null=True, blank=True, default=None)
     unit = models.CharField(max_length=50)
-    place = models.CharField(max_length=50)
-    time = models.TimeField(auto_now_add=False)
+    Parqueadero = models.CharField(max_length=50)
+    Hora llegada = models.TimeField(auto_now_add=False)
+    Hora salida = models.TimeField(auto_now_add=False)
